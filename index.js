851b924c39ef169f163b987689c69cdee82586bc
Edgar Hernandez <zdededg@gmail.com>
2023-06-02 07:39:01 -0700

    completed

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+ 
+// Assuming num1 and num2 are the factors that multiply to 62, 
+// one pair of such factors could be 31 and 2
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2; 
 
-// Write your code here
+// This will generate a random integer greater than 0
+let random = Math.floor(Math.random() * 100) + 1;
+
+// Assuming num3 and num4 are numbers such that num3 divided by num4 gives a remainder of 4, 
+// one such pair could be 24 and 5
+let num3 = 24;
+let num4 = 5;
+let mod = num3 % num4; 
+
+// Assuming we have a set of numbers in an array and we need to find the max
+let numbers = [5, 10, 15, 20]; 
+let max = Math.max(...numbers);
